301a531f035998ac32ea1829b4954676823d116c
Vladislav Ropot <vladislav.ropot.dev@gmail.com>
2025-01-05 16:38:49 +0200

    Add new 3 components: Home, Facts, Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './routes/Home';
+import Facts from './routes/Facts';
+import Details from './routes/Details';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>Facts</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/facts">Facts</Link></li>
         </ul>
+
+        <Routes>
+          <Route path='/' Component={Home}/>
+          <Route path='/facts' Component={Facts}/>
+          <Route path='/details' Component={Details}/>
+        </Routes>
       </nav>
-    </div>
+    </BrowserRouter>
   );
 }
 
