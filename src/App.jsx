0a0d735b99f1bd4354cbc2ef697b0487288f3b8e
Vladislav Ropot <vladislav.ropot.dev@gmail.com>
2025-01-05 16:23:40 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Facts</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
