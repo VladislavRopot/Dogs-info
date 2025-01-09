27288b486c9167194d30bdd58c0056eda4e25c26
Vladislav Ropot <vladislav.ropot.dev@gmail.com>
2025-01-09 13:27:07 +0200

    Change Home.jsx & add new Component Breed.jsx

---
+++ src/components/Breed.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Breed(props) {
+    return (
+        <div>
+            <h3>{props.name}</h3>
+            <p>{props.description}</p>
+            <Link to='details'>See details</Link>
+        </div>
+
+    );
+}
+
+export default Breed;
