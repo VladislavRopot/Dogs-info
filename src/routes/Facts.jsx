51c7d59f300c8f972476393e6ffcdc8680f0a2e1
Vladislav Ropot <vladislav.ropot.dev@gmail.com>
2025-01-22 15:14:26 +0200

    Add new changes in Facts component

--- src/routes/Facts.jsx
+++ src/routes/Facts.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+
 function Facts() {
+    const [facts, setFacts] = useState({});
+    const [inputValue, setInputValue] = useState('');
+    useEffect(() => {
+        const fetchFacts = async () => {
+            const response = await fetch ('https://dogapi.dog/api/v2/facts?limit=5');
+            const data = await response.json();
+
+            setFacts(data);
+        };
+
+        fetchFacts();
+    }, []);
+    
     return (
-        <div>Facts page</div>
+        <>
+            <input type="text" placeholder="Search..." onChange={(event) => setInputValue(event.target.value)}/>
+            {facts.data?.filter(item => item.attributes.body.includes(inputValue)).map(item => (
+                <p key={item.id}>{item.attributes.body}</p>
+            ))}
+        </>
     );
 }
 
